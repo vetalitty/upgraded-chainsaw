9e61c7660e9f13fdccd32712ee3ead4e8c68969b
Vitaly Kuznetsov <vetalitty@gmail.com>
2019-01-20 13:55:12 +0800

    fixed connect issue

--- database/connection.js
+++ database/connection.js
@@ -4,6 +4,6 @@ const { db: { url } } = require('../config');
 mongoose.connect(url);
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
-db.on('error', console.error('MongoDB connection error'));
+db.on('error', (err) => console.error('MongoDB connection error'));
 
 module.exports = db;
